ef327f2d507bdc82935b1191e19777b4e6be7a5b
infon2sp <info.n2sp@gmail.com>
2026-04-22 09:29:31 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,39 @@
-const slides = document.querySelectorAll(".slide");
-let current = 0;
+document.addEventListener("DOMContentLoaded", function () {
 
-function showSlide(index) {
-    slides.forEach(s => s.classList.remove("active"));
-    slides[index].classList.add("active");
-}
+    const slides = document.querySelectorAll(".slide");
+    const slider = document.querySelector(".promo-slider");
 
-// AUTO SLIDE
-setInterval(() => {
-    current = (current + 1) % slides.length;
-    showSlide(current);
-}, 4000);
+    let current = 0;
 
-// SWIPE SUPPORT
-let startX = 0;
-
-const slider = document.querySelector(".promo-slider");
-
-slider.addEventListener("touchstart", e => {
-    startX = e.touches[0].clientX;
-});
-
-slider.addEventListener("touchend", e => {
-    let endX = e.changedTouches[0].clientX;
-    let diff = startX - endX;
-
-    if (diff > 50) {
-        current = (current + 1) % slides.length;
-    } else if (diff < -50) {
-        current = (current - 1 + slides.length) % slides.length;
+    function showSlide(index) {
+        slides.forEach(s => s.classList.remove("active"));
+        slides[index].classList.add("active");
     }
 
-    showSlide(current);
+    // AUTO SLIDE
+    setInterval(() => {
+        current = (current + 1) % slides.length;
+        showSlide(current);
+    }, 4000);
+
+    // SWIPE SUPPORT
+    let startX = 0;
+
+    slider.addEventListener("touchstart", e => {
+        startX = e.touches[0].clientX;
+    });
+
+    slider.addEventListener("touchend", e => {
+        let endX = e.changedTouches[0].clientX;
+        let diff = startX - endX;
+
+        if (diff > 50) {
+            current = (current + 1) % slides.length;
+        } else if (diff < -50) {
+            current = (current - 1 + slides.length) % slides.length;
+        }
+
+        showSlide(current);
+    });
+
 });
